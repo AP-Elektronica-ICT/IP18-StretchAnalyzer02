e12532feedd704e65b50720440d0516d2fbcbd62
Unknown <cedricvanbroek@gmail.com>
2018-03-22 10:23:53 +0100

    notification added

--- app/src/main/java/be/eaict/stretchalyzer2/MainActivity.java
+++ app/src/main/java/be/eaict/stretchalyzer2/MainActivity.java
@@ -1,16 +1,44 @@
 package be.eaict.stretchalyzer2;
 
+import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import java.util.Calendar;
+
 public class MainActivity extends AppCompatActivity {
 
+
+    private int uur = 10;
+    private int minuut = 22;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //notification alarm
+        findViewById(R.id.btnRegister).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar calendar = Calendar.getInstance();
+                calendar.set(Calendar.HOUR_OF_DAY,uur);
+                calendar.set(Calendar.MINUTE,minuut);
+                calendar.set(Calendar.SECOND,1);
+                //intents (pendingintent nodig)
+                Intent intent = new Intent(getApplicationContext(),Notification_reciever.class);
+                PendingIntent pendingIntent = PendingIntent.getBroadcast(getApplicationContext(),100,intent,PendingIntent.FLAG_UPDATE_CURRENT);
+
+                AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+                //rtc_wakeup --> zorgt ervoor dat de notification ook in sleep mode verschijnt.
+                alarmManager.setRepeating(AlarmManager.RTC_WAKEUP,calendar.getTimeInMillis(),AlarmManager.INTERVAL_DAY,pendingIntent);
+            }
+        });
+
     }
 
 

544c293d3462963eb025d7e37a04958799d4bb88
Unknown <cedricvanbroek@gmail.com>
2018-03-25 19:00:48 +0200

    settings activity created

---
+++ app/src/main/java/be/eaict/stretchalyzer2/Settings.java
@@ -0,0 +1,19 @@
+package be.eaict.stretchalyzer2;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by cedric on 25/03/2018.
+ */
+
+public class Settings extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+
+
+}
+}

1f279e8d515e1bddd6ff8bfe88b8464548da739a
Kevin Davis <kevin-davis@hotmail.com>
2018-04-14 19:20:13 +0200

    login in progress

--- app/src/main/java/be/eaict/stretchalyzer2/MainActivity.java
+++ app/src/main/java/be/eaict/stretchalyzer2/MainActivity.java
@@ -2,12 +2,18 @@ package be.eaict.stretchalyzer2;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -20,6 +26,8 @@ import be.eaict.stretchalyzer2.DOM.User;
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
+    private String email;
+    private String password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +39,9 @@ public class MainActivity extends AppCompatActivity {
 
 
         mAuth = FirebaseAuth.getInstance();
+
+
+
     }
 
     @Override
@@ -51,7 +62,59 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onClickLogin(View view) {
-        //checkLogin();
+        checkLogin();
+
+        //WORK IN PROGRESS
+        /*
+        TextView txtEmail = findViewById( R.id.txtName );
+        TextView txtPassword = findViewById( R.id.txtPassword );
+        email = txtEmail.getText().toString().trim();
+        password = txtPassword.getText().toString().trim();
+        if (checkLogin()) {
+            signIn();
+        }else{
+            Toast.makeText(MainActivity.this, "Please fill in all forms.",
+                    Toast.LENGTH_SHORT).show();
+        }
+        */
+
+    }
+
+    private boolean checkLogin() {
+        boolean checked = false;
+
+        if(email == "" || password == ""){
+            checked = true;
+        }
+
+        return checked;
+    }
+
+    private void signIn(){
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("Login", "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            GlobalData.currentUser = user;
+                            updateUI();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("Login", "signInWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI();
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+    private void updateUI() {
         openHomeActivity();
     }
 
